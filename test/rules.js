b65532f1d996cb8d6913e5a2f3b6d005ed745305
Matheus Fernandes <github@matheus.top>
2016-08-05 16:51:57 -0300

    Make tests safer

--- test/rules.js
+++ test/rules.js
@@ -14,33 +14,29 @@ test('badge - incorrect source', async t => {
 });
 
 test('list-item - incorrect item prefix', async t => {
-	const results = (await m({filename: 'fixtures/list-item/1.md'})).messages;
-	for (const result of results) {
-		t.is(result.ruleId, 'awesome-list-item');
-		t.is(result.message, 'List items must start with `- [name](link)`');
-	}
+	t.plan(2);
+	let messages = (await m({filename: 'fixtures/list-item/1.md'})).messages;
+	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
+	messages.forEach(message => t.is(message.message, 'List items must start with `- [name](link)`'));
 });
 
 test('list-item - missing dash between link and description', async t => {
-	const results = (await m({filename: 'fixtures/list-item/2.md'})).messages;
-	for (const result of results) {
-		t.is(result.ruleId, 'awesome-list-item');
-		t.is(result.message, 'List items must have a ` - ` between the link and the description');
-	}
+	t.plan(1);
+	let messages = (await m({filename: 'fixtures/list-item/2.md'})).messages;
+	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
+	messages.forEach(message => t.is(message.message, 'List items must have a ` - ` between the link and the description'));
 });
 
 test('list-item - description does not starts with camelCase, uppercase or `code`', async t => {
-	const results = (await m({filename: 'fixtures/list-item/3.md'})).messages;
-	for (const result of results) {
-		t.is(result.ruleId, 'awesome-list-item');
-		t.is(result.message, 'The description must start with an uppercase, camelCase world or `code`');
-	}
+	t.plan(2);
+	let messages = (await m({filename: 'fixtures/list-item/3.md'})).messages;
+	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
+	messages.forEach(message => t.is(message.message, 'The description must start with an uppercase, camelCase world or `code`'));
 });
 
 test('list-item – description must end with a . or !', async t => {
-	const results = (await m({filename: 'fixtures/list-item/4.md'})).messages;
-	for (const result of results) {
-		t.is(result.ruleId, 'awesome-list-item');
-		t.is(result.message, 'The description of a list item must end with `.` or `!`');
-	}
+	t.plan(2);
+	let messages = (await m({filename: 'fixtures/list-item/4.md'})).messages;
+	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
+	messages.forEach(message => t.is(message.message, 'The description of a list item must end with `.` or `!`'));
 });
