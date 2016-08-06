278fdb89a7224f37682b2897f1fb3a62f84a0746
Matheus Fernandes <github@matheus.top>
2016-08-06 19:19:42 -0300

    Change rule name convention (#12)

--- test/rules.js
+++ test/rules.js
@@ -3,12 +3,12 @@ import m from '../';
 
 test('badge - missing', async t => {
 	const result = (await m({filename: 'fixtures/badge.md'})).messages[0];
-	t.is(result.ruleId, 'awesome-badge');
+	t.is(result.ruleId, 'awesome/badge');
 	t.is(result.message, 'Missing Awesome badge after the main heading');
 });
 
 test('badge - incorrect source', async t => {
 	const result = (await m({filename: 'fixtures/badge2.md'})).messages[0];
-	t.is(result.ruleId, 'awesome-badge');
+	t.is(result.ruleId, 'awesome/badge');
 	t.is(result.message, 'Incorrect badge source');
 });
