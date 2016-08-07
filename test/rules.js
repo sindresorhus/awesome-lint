18ffbaafc306d8fea2e66e15e00a558269f56ccb
Matheus Fernandes <github@matheus.top>
2016-08-07 00:11:49 -0300

    Reduce code repetition in tests

--- test/rules.js
+++ test/rules.js
@@ -1,6 +1,11 @@
 import test from 'ava';
 import m from '../';
 
+async function run(fileName, ruleId) {
+	let results = (await m({filename: fileName})).messages;
+	return results.filter(result => result.ruleId === ruleId);
+}
+
 test('badge - missing', async t => {
 	const result = (await m({filename: 'fixtures/badge.md'})).messages[0];
 	t.is(result.ruleId, 'awesome-badge');
@@ -14,48 +19,51 @@ test('badge - incorrect source', async t => {
 });
 
 test('list-item - incorrect item prefix', async t => {
-	t.plan(2);
-	let messages = (await m({filename: 'fixtures/list-item/1.md'})).messages;
-	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
-	messages.forEach(message => t.is(message.message, 'List items must start with `- [name](link)`'));
+	let results = await run('fixtures/list-item/1.md', 'awesome-list-item');
+	t.is(results.length, 2);
+	for (const result of results) {
+		t.is(result.message, 'List items must start with `- [name](link)`');
+	}
 });
 
 test('list-item - missing dash between link and description', async t => {
-	t.plan(1);
-	let messages = (await m({filename: 'fixtures/list-item/2.md'})).messages;
-	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
-	messages.forEach(message => t.is(message.message, 'List items must have a ` - ` between the link and the description'));
+	let results = await run('fixtures/list-item/2.md', 'awesome-list-item');
+	t.is(results.length, 1);
+	for (const result of results) {
+		t.is(result.message, 'List items must have a ` - ` between the link and the description');
+	}
 });
 
 test('list-item - description does not starts with camelCase, uppercase or `code`', async t => {
-	t.plan(2);
-	let messages = (await m({filename: 'fixtures/list-item/3.md'})).messages;
-	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
-	messages.forEach(message => t.is(message.message, 'The description must start with an uppercase, camelCase word or `code`'));
+	let results = await run('fixtures/list-item/3.md', 'awesome-list-item');
+	t.is(results.length, 2);
+	for (const result of results) {
+		t.is(result.message, 'The description must start with an uppercase, camelCase word or `code`');
+	}
 });
 
 test('list-item – description must end with a . or !', async t => {
-	t.plan(2);
-	let messages = (await m({filename: 'fixtures/list-item/4.md'})).messages;
-	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
-	messages.forEach(message => t.is(message.message, 'The description of a list item must end with `.` or `!`'));
+	let results = await run('fixtures/list-item/4.md', 'awesome-list-item');
+	t.is(results.length, 2);
+	for (const result of results) {
+		t.is(result.message, 'The description of a list item must end with `.` or `!`');
+	}
 });
 
 test('list-item – nested lists', async t => {
-	let messages = (await m({filename: 'fixtures/list-item/5.md'})).messages;
-	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
-	t.is(messages.length, 0);
+	let results = await run('fixtures/list-item/5.md', 'awesome-list-item');
+	t.is(results.length, 0);
 });
 
 test('list-item – `Contents` section', async t => {
-	let messages = (await m({filename: 'fixtures/list-item/6.md'})).messages;
-	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
-	t.is(messages.length, 0);
+	let results = await run('fixtures/list-item/6.md', 'awesome-list-item');
+	t.is(results.length, 0);
 });
 
-test('list-item – item w/o description', async t => {
-	t.plan(2);
-	let messages = (await m({filename: 'fixtures/list-item/7.md'})).messages;
-	messages = messages.filter(message => message.ruleId === 'awesome-list-item');
-	messages.forEach(message => t.is(message.message, 'List items must have a description'));
+test('list-item – item without description', async t => {
+	let results = await run('fixtures/list-item/7.md', 'awesome-list-item');
+	t.is(results.length, 2);
+	for (const result of results) {
+		t.is(result.message, 'List items must have a description');
+	}
 });
