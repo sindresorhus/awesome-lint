bc2e79eb6c755faacd44d4a36c66ccc2e9ec48f6
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-04-05 21:24:12 -0700

    fix test errors from linting issues (for issue #15)

--- test/cli.js
+++ test/cli.js
@@ -2,6 +2,6 @@ import test from 'ava';
 import execa from 'execa';
 
 test(async t => {
-	const err = await t.throws(execa.stdout('../cli.js', ['fixtures/main.md']));
+	const err = await t.throws(execa.stdout('./cli.js', ['test/fixtures/main.md']));
 	t.regex(err.message, /Missing Awesome badge/);
 });
--- test/rules.js
+++ test/rules.js
@@ -2,13 +2,13 @@ import test from 'ava';
 import m from '../';
 
 test('badge - missing', async t => {
-	const result = (await m({filename: 'fixtures/badge.md'})).messages[0];
+	const result = (await m({filename: 'test/fixtures/badge.md'})).messages[0];
 	t.is(result.ruleId, 'awesome/badge');
 	t.is(result.message, 'Missing Awesome badge after the main heading');
 });
 
 test('badge - incorrect source', async t => {
-	const result = (await m({filename: 'fixtures/badge2.md'})).messages[0];
+	const result = (await m({filename: 'test/fixtures/badge2.md'})).messages[0];
 	t.is(result.ruleId, 'awesome/badge');
 	t.is(result.message, 'Incorrect badge source');
 });
