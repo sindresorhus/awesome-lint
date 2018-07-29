4a5bdf0b84aaa7c4cbf293e4d44c0f7e32574a86
Travis Fischer <fisch0920@gmail.com>
2018-07-29 11:16:05 -0400

    relax rule awesome/badge url matching
    fix tests without toc

--- rules/badge.js
+++ rules/badge.js
@@ -2,26 +2,35 @@
 const rule = require('unified-lint-rule');
 const visit = require('unist-util-visit');
 
+const badgeSrcRe = /^https:\/\/cdn\.rawgit\.com\/sindresorhus\/awesome\/\w*\/media\/badge.svg$/i;
+
+const badgeUrlWhitelist = new Set([
+	'https://awesome.re',
+	'https://github.com/sindresorhus/awesome'
+]);
+
+const badgeSrcUrlWhitelist = new Set([
+	'https://awesome.re/badge.svg',
+	'https://awesome.re/badge-flat.svg'
+]);
+
 module.exports = rule('remark-lint:awesome/badge', (ast, file) => {
 	visit(ast, 'heading', (node, index) => {
 		if (index > 0) {
 			return;
 		}
 
-		const badgeUrl = 'https://awesome.re';
-		const badgeSrcUrl = 'https://awesome.re/badge.svg';
-
 		let hasBadge = false;
 
 		for (const child of node.children) {
-			if (node.depth !== 1 || child.type !== 'link' || child.url !== badgeUrl) {
+			if (node.depth !== 1 || child.type !== 'link' || !isValidBadgeUrl(child.url)) {
 				continue;
 			}
 
 			for (const child2 of child.children) {
 				if (child2.type === 'image') {
-					if (child2.url !== badgeSrcUrl) {
-						file.message('Incorrect badge source', child2);
+					if (!isValidBadgeSrcUrl(child2.url)) {
+						file.message('Invalid badge source', child2);
 						return;
 					}
 
@@ -35,3 +44,11 @@ module.exports = rule('remark-lint:awesome/badge', (ast, file) => {
 		}
 	});
 });
+
+function isValidBadgeUrl(url) {
+	return badgeUrlWhitelist.has(url);
+}
+
+function isValidBadgeSrcUrl(url) {
+	return badgeSrcUrlWhitelist.has(url) || badgeSrcRe.test(url);
+}
---
+++ test/fixtures/badge-success0.md
@@ -0,0 +1,15 @@
+# Foo [![Awesome](https://awesome.re/badge.svg)](https://awesome.re)
+
+> Bar
+
+## Contents
+
+- [Foo](#foo)
+
+## Foo
+
+non-empty
+
+## License
+
+non-empty
