b4d321b7e649269f3735351ab140af3876d2307c
Travis Fischer <fisch0920@gmail.com>
2018-07-29 11:16:05 -0400

    relax rule awesome/badge url matching

---
+++ test/fixtures/badge-success0.md
@@ -0,0 +1,3 @@
+# Foo [![Awesome](https://awesome.re/badge.svg)](https://awesome.re)
+
+> Bar
--- test/rules.js
+++ test/rules.js
@@ -2,13 +2,25 @@ import test from 'ava';
 import m from '..';
 
 test('badge - missing', async t => {
-	const result = (await m({filename: 'test/fixtures/badge.md'})).messages[0];
+	const result = (await m({filename: 'test/fixtures/badge0.md'})).messages[0];
 	t.is(result.ruleId, 'awesome/badge');
 	t.is(result.message, 'Missing Awesome badge after the main heading');
 });
 
 test('badge - incorrect source', async t => {
-	const result = (await m({filename: 'test/fixtures/badge2.md'})).messages[0];
+	const result = (await m({filename: 'test/fixtures/badge1.md'})).messages[0];
 	t.is(result.ruleId, 'awesome/badge');
-	t.is(result.message, 'Incorrect badge source');
+	t.is(result.message, 'Invalid badge source');
+});
+
+test('badge - success (short)', async t => {
+	const result = (await m({filename: 'test/fixtures/badge-success0.md'})).messages[0];
+	t.is(result.ruleId, 'awesome/toc');
+	t.is(result.message, 'Missing or invalid Table of Contents');
+});
+
+test('badge - success (long)', async t => {
+	const result = (await m({filename: 'test/fixtures/badge-success1.md'})).messages[0];
+	t.is(result.ruleId, 'awesome/toc');
+	t.is(result.message, 'Missing or invalid Table of Contents');
 });
