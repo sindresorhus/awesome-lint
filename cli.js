8b67b7df286c8ee099b122b567f3f3332271d2e8
Travis Fischer <fisch0920@gmail.com>
2018-09-11 08:00:17 -0400

    Refactor CLI and main to allow programmatic usage

--- cli.js
+++ cli.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 'use strict';
-const gitClone = require('git-clone');
-const isUrl = require('is-url-superb');
 const meow = require('meow');
-const pify = require('pify');
-const rmfr = require('rmfr');
-const tempy = require('tempy');
 
 const findReadmeFile = require('./lib/find-readme-file');
 const awesomeLint = require('.');
@@ -18,32 +13,14 @@ const main = async () => {
 
 	const options = { };
 	const input = cli.input[0];
-	let temp = null;
 
 	if (input) {
-		if (isUrl(input)) {
-			temp = tempy.directory();
-			await pify(gitClone)(input, temp);
-
-			const readme = findReadmeFile(temp);
-			if (!readme) {
-				await rmfr(temp);
-				throw new Error(`Unable to find valid readme for "${input}"`);
-			}
-
-			options.filename = readme;
-		} else {
-			options.filename = input;
-		}
+		options.filename = input;
 	} else {
-		options.filename = 'readme.md';
+		options.filename = findReadmeFile(process.cwd());
 	}
 
 	await awesomeLint.report(options);
-
-	if (temp) {
-		await rmfr(temp);
-	}
 };
 
 main();
