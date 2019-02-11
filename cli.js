b60de420fb190e0fd0424a108ba9ea23d73a8b66
Danny McCormick <damccorm@microsoft.com>
2019-02-11 10:32:39 -0500

    Add reporter option to cli

--- cli.js
+++ cli.js
@@ -8,16 +8,40 @@ const awesomeLint = require('.');
 const main = async () => {
 	const cli = meow(`
 		Usage
-			$ awesome-lint
-	`);
+			$ awesome-lint <optional input filename>
+		
+		Options
+			--reporter, -r Use a custom reporter
+	`, {
+		flags: {
+			reporter: {
+				type: 'string',
+				alias: 'r'
+			}
+		}
+	});
 
 	const options = { };
 	const input = cli.input[0];
+	const reporterName = cli.flags.reporter;
 
 	if (input) {
 		options.filename = input;
 	} else {
 		options.filename = findReadmeFile(process.cwd());
+	}
+
+	if (reporterName) {
+		// Check if reporter is an npm package
+		try {
+			options.reporter = require(reporterName).report;
+		} catch (error) {
+			if (error.code === 'MODULE_NOT_FOUND') {
+				console.log(`No reporter found matching "${reporterName}". Proceeding with default reporter (vfile-reporter-pretty)`);
+			} else {
+				throw error;
+			}
+		}
 	}
 
 	await awesomeLint.report(options);
