4890b0b1342a1ab07f42543375e95aceefe541c1
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-13 14:48:32 +0700

    Update api.js

--- test/api.js
+++ test/api.js
@@ -5,14 +5,14 @@ test('main', async t => {
 	t.true((await m({filename: 'test/fixtures/main.md'})).messages.length > 0);
 });
 
-test('custom reporter', async t => {
-	let reporterCalled = false;
-	const customReporter = function (vFileArr) {
-		if (vFileArr.length > 0) {
-			reporterCalled = true;
+test('`reporter` option', async t => {
+	let wasReporterCalled = false;
+	const reporter = reports => {
+		if (reports.length > 0) {
+			wasReporterCalled = true;
 		}
 	};
 
-	await m.report({filename: 'test/fixtures/main.md', reporter: customReporter});
-	t.true(reporterCalled);
+	await m.report({filename: 'test/fixtures/main.md', reporter});
+	t.true(wasReporterCalled);
 });
