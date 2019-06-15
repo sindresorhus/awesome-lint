5275969979facac34aeaae3a931e30a99ff8d54d
Sindre Sorhus <sindresorhus@gmail.com>
2019-06-16 00:57:23 +0700

    Update git-repo-age.js

--- rules/git-repo-age.js
+++ rules/git-repo-age.js
@@ -35,9 +35,8 @@ module.exports = rule('remark-lint:awesome/git-repo-age', async (ast, file) => {
 			file.message(`Git repository must be at least ${minGitRepoAgeDays} days old`);
 		}
 	} catch (_) {
-		// Not a Git repository or a shallow copy missing the
-		// .travis.yml "git:\ndepth: false" setting
-		file.message('Awesome list must reside in a valid deep-cloned git repository (see https://github.com/sindresorhus/awesome-lint#tip for more information)');
+		// Not a Git repository or a shallow copy missing the `.travis.yml` `git:\ndepth: false` setting
+		file.message('Awesome list must reside in a valid deep-cloned Git repository (see https://github.com/sindresorhus/awesome-lint#tip for more information)');
 	}
 });
 
