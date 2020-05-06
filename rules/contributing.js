a427f269d3280b356035f8f4f2ff2362a9da0092
Kevin Deldycke <kevin@deldycke.com>
2020-05-06 10:53:45 +0200

    Allow `.github` directory to contain contribution guidelines (#102)

--- rules/contributing.js
+++ rules/contributing.js
@@ -7,7 +7,7 @@ const rule = require('unified-lint-rule');
 module.exports = rule('remark-lint:awesome-contributing', (ast, file) => {
 	const {dirname} = file;
 
-	const contributingFile = globby.sync(['contributing.md', 'CONTRIBUTING.md'], {cwd: dirname})[0];
+	const contributingFile = globby.sync(['{.github/,}contributing.md'], {nocase: true, cwd: dirname})[0];
 	// TODO: This doesn't work on Linux for some reason. Investigate and then open an issue on `fast-glob`.
 	// const contributingFile = globby.sync('contributing.md', {case: false, cwd: dirname})[0];
 
