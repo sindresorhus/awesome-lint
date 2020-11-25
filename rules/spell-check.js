004cec135d3c5f6904fd0c1137da00d350306fdf
Brandon Smith <4730164+brandon93s@users.noreply.github.com>
2020-11-25 16:06:14 -0500

    Skip URLs in spell check rule

--- rules/spell-check.js
+++ rules/spell-check.js
@@ -1,4 +1,5 @@
 'use strict';
+const isUrl = require('is-url-superb');
 const rule = require('unified-lint-rule');
 const visit = require('unist-util-visit');
 const arrify = require('arrify');
@@ -14,20 +15,29 @@ module.exports = rule('remark-lint:awesome-spell-check', (ast, file) => {
 			return;
 		}
 
+		const sanitizedSegments = [];
+		for (const segment of node.value.split(/\s/g)) {
+			if (!isUrl(segment)) {
+				sanitizedSegments.push(segment);
+			}
+		}
+
+		const sanitizedValue = sanitizedSegments.join(' ');
+
 		for (const rule of spellCheckRules) {
 			const {test, value} = rule;
 			const regs = arrify(test).map(reg => new RegExp(reg));
 
 			for (const re of regs) {
 				for (;;) {
-					const match = re.exec(node.value);
+					const match = re.exec(sanitizedValue);
 					if (!match) {
 						break;
 					}
 
 					if (match[0] !== value) {
-						const previousCharacter = node.value[match.index - 1];
-						const nextCharacter = node.value[match.index + match[0].length];
+						const previousCharacter = sanitizedValue[match.index - 1];
+						const nextCharacter = sanitizedValue[match.index + match[0].length];
 
 						if (wordBreakCharacterWhitelist.has(previousCharacter)) {
 							continue;
